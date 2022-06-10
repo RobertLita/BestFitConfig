60dd55446f0a64b089d83be9dafc34cbade1d5d1
andreidina <andreiconstantindina@gmail.com>
2022-06-10 18:38:52 +0300

    Am adaugat functiile de add in repository

--- ProiectMDS/ProiectMDS/RepositoryClienti.h
+++ ProiectMDS/ProiectMDS/RepositoryClienti.h
@@ -1,5 +1,22 @@
 #pragma once
+
+#include <vector>
+#include <string>
+#include "Client.h"
+
+using std::vector, std::string;
+
 class RepositoryClienti
 {
+private:
+	vector <Client> clienti;
+
+	RepositoryClienti();
+
+	void addClient(Client c) {
+		this->clienti.push_back(c);
+	}
+
+
 };
 
--- ProiectMDS/ProiectMDS/RepositoryFacilitati.h
+++ ProiectMDS/ProiectMDS/RepositoryFacilitati.h
@@ -1,5 +1,22 @@
 #pragma once
+
+#include <vector>
+#include <string>
+#include "Facilitate.h"
+
+using std::vector, std::string;
+
 class RepositoryFacilitati
 {
+private:
+	vector <Facilitate> facilitati;
+
+	RepositoryFacilitati();
+
+	void addClient(Facilitate f) {
+		this->facilitati.push_back(f);
+	}
+
+
 };
 
